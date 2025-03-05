0a9c55710d43297d6a824c442b6fc0d3e4b9a049
Aparnakavitha <aparnakavitha1@gmail.com>
2025-03-05 09:26:48 +0530

    backend firstedit

--- src/main/java/com/leaderboard/demo/repository/UserRepository.java
+++ src/main/java/com/leaderboard/demo/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.leaderboard.demo.repository;
 import com.leaderboard.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    List<User> findByIsDeletedFalse();
+    default Optional<User> findByEmail(String email) {
+        return null;
+    }
 }
